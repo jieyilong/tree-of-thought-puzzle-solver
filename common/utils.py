e15075a27ed0c482877481fe4223a5f7d5f6025c
Jieyi Long (THETA Network) <jieyi@sliver.tv>
2023-04-19 23:22:32 +0800

    Minor fixes

--- common/utils.py
+++ common/utils.py
@@ -1,9 +1,9 @@
 import json
 
 def extract_json_from_text_string(text_str: str):
-    lp_idx = text_str.index('{')
-    rp_idx = text_str.rindex('}')
     try:
+        lp_idx = text_str.index('{')
+        rp_idx = text_str.rindex('}')
         json_str = text_str[lp_idx:rp_idx+1]
         json_obj = json.loads(json_str)
         return True, json_obj 
