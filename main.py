f8f76d4ccb4e39be2a632e58fea89b17a30021cc
Jieyi Long (THETA Network) <jieyi@sliver.tv>
2023-04-20 23:10:53 +0800

    Update test cases

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ if __name__ == "__main__":
     #user_input = "please solve this 4x4 sudoku puzzle [[3,*,*,2],[1,*,3,*],[*,1,*,3],[4,*,*,1]]."
 
     # GOOD TEST CASE: even the ChatGPT web cannot solve these
-    #user_input = "please solve this 5x5 sudoku puzzle [[*,1,*,*,*],[*,*,3,*,2],[*,*,*,3,*],[*,4,*,1,*],[*,*,1,*,*]]."
-    user_input = "please solve this 4x4 sudoku puzzle [[*,1,*,*],[*,*,2,*],[*,*,*,4],[*,*,*,1]]."
+    #user_input = "please solve this 5x5 sudoku puzzle [[*,1,*,*,*],[*,*,3,*,2],[*,*,*,3,*],[*,4,*,1,*],[*,*,1,*,*]] where * meaning an empty cell to be filled in."
+   
+    # GOOD TEST CASE: Each row/column of this puzzle has 2-3 unknowns, which seems to be a sweetspot for ToT vs. ChatGPT
+    user_input = "please solve this 5x5 sudoku puzzle [[*,1,*,5,4],[1,*,3,*,2],[5,2,*,3,*],[*,4,*,1,*],[4,*,1,*,5]] where * meaning an empty cell to be filled in."
+    
+    #user_input = "please solve this 4x4 sudoku puzzle [[*,1,*,*],[*,*,2,*],[*,*,*,4],[1,*,*,*]]."
+    #user_input = "please solve this 4x4 sudoku puzzle [[2,1,*,*],[*,*,3,*],[*,*,*,4],[4,*,*,*]], where * meaning an empty cell to be filled in."
     
     #user_input = "Please solve this 9x9 Sudoku puzzle: [[1, *, *, 2, *, *, *, 9, *], [*, *, 5, *, *, 6, *, *, *], [*, 4, *, 1, *, *, *, *, *], [*, *, 3, *, 5, *, *, *, *], [*, *, *, *, 1, 2, *, *, *], [*, *, *, *, *, *, *, *, *], [*, *, *, *, *, *, *, 8, *], [*, *, *, *, *, 8, *, *, *], [*, *, *, *, *, 7, *, *, *]]."
     #user_input = "Please solve this 9x9 Sudoku puzzle: [[4,2,*,9,*,3,8,*,6],[9,1,*,*,8,*,2,*,4],[6,*,3,*,5,*,*,*,9],[*,7,1,*,4,*,6,9,*],[3,*,9,*,2,6,*,8,5],[*,*,*,*,9,*,3,*,*],[*,9,*,*,6,*,4,*,*],[1,*,*,*,*,9,5,*,*],[7,*,4,5,*,8,*,*,*]]."
